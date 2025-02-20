d22dd8d788931b21502fc604576f80f31cbf6038
njlyon0 <nicholasjlyon@gmail.com>
2025-02-20 16:02:19 -0500

    feat (harmonize): first pass at harmonization workflow

---
+++ 01_harmonize.R
@@ -0,0 +1,125 @@
+## ----------------------------------------------------------------- ##
+# Resilience Management - Harmonization Workflow
+## ----------------------------------------------------------------- ##
+# Authors: Nick J Lyon, ...
+
+# Purpose
+## Ingest all raw data files and harmonize them into a single data table
+## "Harmonize" = combine comparable columns and standardize column names
+
+## --------------------------------------- ##
+# Housekeeping ----
+## --------------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, ltertools, googledrive, supportR)
+
+# Make needed folder(s)
+dir.create(file.path("data"), showWarnings = F)
+dir.create(file.path("data", "raw"), showWarnings = F)
+dir.create(file.path("data", "tidy"), showWarnings = F)
+
+# Clear environment + collect garbage
+rm(list = ls()); gc()
+
+## ------------------------------------------- ##
+# Download Data ----
+## ------------------------------------------- ##
+
+# NOTE
+## This script assumes (1) access to the "LTER-WG_Resilience-Management" Shared Drive (2) authentication with R
+## For more information on authentication, see the following tutorial:
+### https://lter.github.io/scicomp/tutorial_googledrive-pkg.html
+
+# Identify wanted files
+files_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1zI1KYBlROyBZSgjSEYmVjsIfCmRPpUPq")) %>% 
+  dplyr::filter(stringr::str_detect(string = .$name, pattern = "\\.csv"))
+
+# Did that work?
+files_drive
+
+# Identify local files
+files_local <- dir(path = file.path("data", "raw"))
+files_local
+
+# Overwrite local data files?
+update <- FALSE
+
+# Identify desired files
+if(update == T) {
+  files_wanted <- files_drive 
+} else {
+  files_wanted <- files_drive %>%
+    dplyr::filter(!name %in% files_local)
+}
+
+# Download them!
+purrr::walk2(.x = files_wanted$id, .y = files_wanted$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", "raw", .y)))
+
+# Grab the data key
+key_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Ty7QX7vyvD797eKJzMWbr8AwIo-GyBFO")) %>% 
+  dplyr::filter(name == "resilience_data-key")
+
+# Did that work?
+key_drive
+
+# Download the data key
+googledrive::drive_download(file = key_drive$id, overwrite = T, type = "csv",
+                            path = file.path("data", key_drive$name))
+
+## ------------------------------------------- ##
+# Harmonize! ----
+## ------------------------------------------- ##
+
+# Read in data key
+key <- read.csv(file = file.path("data", "resilience_data-key.csv"))
+
+# Check that looks roughly right
+dplyr::glimpse(key)
+
+# Perform harmonization
+combo_v1 <- ltertools::harmonize(key = key, raw_folder = file.path("data", "raw"),
+                                 data_format = "csv", quiet = F)
+
+# Check that structure out
+dplyr::glimpse(combo_v1)
+
+## ------------------------------------------- ##
+# Re-Order Columns ----
+## ------------------------------------------- ##
+
+# Want columns to be ordered more intuitively
+combo_v2 <- combo_v1 %>% 
+  # Site information towards the left
+  dplyr::relocate(lter, site, block, plot, quadrat, .after = year) %>% 
+  # ANPP/NPP to the far right
+  dplyr::relocate(dplyr::contains("npp_"), .after = dplyr::everything())
+
+# Make sure no columns are unexpectedly lost/gained
+supportR::diff_check(old = names(combo_v1), new = names(combo_v2))
+
+# Check structure 
+dplyr::glimpse(combo_v2)
+
+## ------------------------------------------- ##
+# Export ----
+## ------------------------------------------- ##
+
+# Final pre-export tweaks
+combo_v99 <- combo_v2
+
+# Check structure
+dplyr::glimpse(combo_v99)
+
+# Export locally
+write.csv(x = combo_v99, row.names = F, na = '',
+          file = file.path("data", "tidy", "01_resilience_harmonized.csv"))
+
+# Upload to Drive
+googledrive::drive_upload(media = file.path("data", "tidy", "01_resilience_harmonized.csv"), overwrite = T,
+                          path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/13Ymkrr-kRLDmpaj1jwwVOnOSmEYnF-dJ"))
+
+
+# End ----
