666b04c5d4a2b5c2d0af6e87e81b4b965e04044f
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2025-02-25 15:12:45 -0700

    updated all file paths and I think all sites completed

--- pre-process/LTER_pre-process.R
+++ pre-process/LTER_pre-process.R
@@ -7,7 +7,7 @@ library(plyr)
 
 
 ##Sev
-sev_anpp <- read.csv("C:/Users/ohler/Downloads/sev182_NPP_core_biomass.csv")%>%
+sev_anpp <- read.csv("G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/sev182_NPP_core_biomass.csv")%>%
   subset(site == "core_black" & season == "fall")%>% #picked black grama core site. fall season is major growing season
   dplyr::select(site, year, web, plot, quad, kartez, biomass.BM)%>%
   ddply(.(site, year, web, plot, quad), function(x)data.frame(
@@ -15,12 +15,12 @@ sev_anpp <- read.csv("C:/Users/ohler/Downloads/sev182_NPP_core_biomass.csv")%>%
   ))
 sev_anpp$site <- "sev_lter"
 
-write.csv(sev_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/sev_anpp_nceas.csv")
+write.csv(sev_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data_to_use/sev_anpp_nceas.csv")
 
 rm(list = ls()); gc()
 
 ##Jornada
-jornada_anpp <- read.csv("C:/Users/ohler/Downloads/Ecotone_ANPP_by_Site.csv")
+jornada_anpp <- read.csv("G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/Ecotone_ANPP_by_Site.csv")
 
 jornada_anpp$zone <- revalue(jornada_anpp$zone, c(S = "shrubland", E = "ecotone", G = "grassland"))
 
@@ -31,12 +31,12 @@ jornada_anpp$location <- jornada_anpp$site
 jornada_anpp$site <- "jrn_lter"
 
 
-write.csv(jornada_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/jrn_anpp_nceas.csv")
+write.csv(jornada_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data_to_use/jrn_anpp_nceas.csv")
 
 rm(list = ls()); gc()
 
 ##Konza
-konza_anpp <- read.csv("C:/Users/ohler/Downloads/PAB011.csv")
+konza_anpp <- read.csv("G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/PAB011.csv")
 konza_anpp$anpp <- (konza_anpp$LVGRASS + konza_anpp$FORBS)*10 #make per meter squared
 konza_anpp <- konza_anpp%>%
               unite("date", c("RECYEAR","RECMONTH","RECDAY"), sep = "-",remove = TRUE)
@@ -53,28 +53,31 @@ konza_anpp <- konza_anpp%>%
 
 konza_anpp$site <- "knz_lter"
 
-write.csv(konza_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/knz_anpp_nceas.csv")
+write.csv(konza_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data_to_use/knz_anpp_nceas.csv")
 
 rm(list = ls()); gc()
 
 
 
 ##Niwot
-niwot_anpp <- read.csv("C:/Users/ohler/Downloads/saddgrid_npp.hh.data.csv")%>%
+niwot_anpp <- read.csv("G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/saddgrid_npp.hh.data.csv")%>%
   dplyr::select(LTER_site, local_site, year, collection_date,veg_class, grid_pt, subsample, NPP)
 
-write.csv(niwot_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/nwt_anpp_nceas.csv")
+write.csv(niwot_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data_to_use/nwt_anpp_nceas.csv")
 #pick vegclass FF=fellfield, DM=dry meadow, MM=moist meadow, ST=shrub tundra, SB=snowbed, WM=wet meadow, SF=snowfence. 
 
 rm(list = ls()); gc()
 
 ##Cedar Creek
-cdr_anpp <- read.csv("C:/Users/ohler/Downloads/E001_Aboveground_Biomass.csv")%>%
+cdr_anpp <- read.csv("G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data/E001_Aboveground_Biomass.csv")%>%#field C whole gets burned semi-regularly #data from 
   subset(NTrt == "9")%>%
   ddply(.(Year, Field, Plot),function(x)data.frame(
     anpp = sum(x$Biomass.g.m2)
   ))
 
-write.csv(cdr_anpp, "C:/Users/ohler/Dropbox/Tim Work/cdr_anpp_nceas.csv")
+cdr_anpp$burn_trt <- ifelse(cdr_anpp$Field == "C", "burn","control")
+cdr_anpp$site <- "cdr_lter"
+
+write.csv(cdr_anpp, "G:/Shared drives/LTER-WG_Resilience-Management/data/raw_data_to_use/cdr_anpp_nceas.csv")
 
 rm(list = ls()); gc()kg
