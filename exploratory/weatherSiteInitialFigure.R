6edacab92dfaa9d7a34d05aefff89bf293cc12fc
= <=>
2025-02-26 14:54:50 -0500

    made some initial weather figures

--- exploratory/weatherSiteInitialFigure.R
+++ exploratory/weatherSiteInitialFigure.R
@@ -21,11 +21,42 @@ purrr::walk2(.x = weather_drive$name,
                                          path = file.path("data","tidy_data", .x)))
             
 maw <- read.csv("data/tidy_data/daymet_meanannual_weather.csv")
+daily <- read.csv("data/tidy_data/")
 
 # make plot------
+## overlay on whitaker
+#devtools::install_github("valentinitnelav/plotbiomes")
+library(plotbiomes)
+maw$mean_precip_cmyear <- maw$mean_precip_mmyear/10
+whitplot <- plotbiomes::whittaker_base_plot() + 
+  geom_point(data = maw, mapping = aes(x = mean_tmean_degC,
+                                       y = mean_precip_cmyear, color = network)) 
 
-ggplot(maw,aes(x = mean_tmax_degC, 
-               y = mean_precip_mmyear, 
-               color = network,
-               label = site_id)) + geom_point()+geom_label()
-  
+ggsave(filename = "data/exploratory_graphs/whittakerplot.png",
+       plot = whitplot,width = 8, height = 6,units = "in",dpi = 300)
+
+googledrive::drive_upload(media = file.path("data", "exploratory_graphs","whittakerplot.png"), overwrite = T,
+                          path = googledrive::as_id(dir.exploratory_graphs))
+
+## histograms.
+library(ggpubr)
+p1 <- ggplot(maw) + geom_histogram(aes(x = mean_precip_mmyear,
+                               color = network), fill = NA, lwd=2.3) 
+p2 <- ggplot(maw) + geom_histogram(aes(x = mean_tmean_degC,
+                               color = network), fill=NA, lwd=2.3) 
+
+
+  ggarrange(p1,p2)
+
+p1 <- ggplot(maw) + geom_dotplot(aes(x = mean_precip_mmyear,y = network,
+                                      fill = network) ,binwidth=50, stackdir = "center")
+p2 <- ggplot(maw) + geom_dotplot(aes(x = mean_tmean_degC,y = network,
+                                   fill = network) ,binwidth=1, stackdir = "center")
+
+dplot <- ggarrange(p1,p2)
+
+ggsave(filename = "data/exploratory_graphs/mat_map_dotplot.png",
+       plot = dplot,width = 12, height = 6,units = "in",dpi = 300)
+
+googledrive::drive_upload(media = file.path("data", "exploratory_graphs","mat_map_dotplot.png"), overwrite = T,
+                          path = googledrive::as_id(dir.exploratory_graphs))
