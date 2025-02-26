f1b69344a29aeaeb5a4e5ea94bbf2e57e9c516a4
LinaAoyama <lina.peninsula@gmail.com>
2025-02-26 15:16:03 -0800

    still in process calculating PET

--- enviro-covariates/aridity_index.R
+++ enviro-covariates/aridity_index.R
@@ -2,4 +2,12 @@
 #Aridity index defined as long-term mean precipitation over 
 #potential evapotranspiration (P/PET)
 
+#load packages
+library(tidyverse)
+library(SPEI)
+
+#download climate data and site info (see weatherSiteInitialFigure.R)
+#merge climate and site tables
+climate_site <- left_join(site_info, maw) %>%
+  mutate(PET = SPEI::thornthwaite(climate_site$mean_tmean_degC, climate_site$latitude, na.rm = FALSE))#calculate PET
 
