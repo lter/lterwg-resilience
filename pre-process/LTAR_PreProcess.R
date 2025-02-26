71ac3395593945da81633552c090572cb7ad158e
oliviahaj <olhajek@gmail.com>
2025-02-25 16:57:38 -0800

    Add site names for half of LTAR sites

--- pre-process/LTAR_PreProcess.R
+++ pre-process/LTAR_PreProcess.R
@@ -4,35 +4,106 @@
 library(tidyverse)
 library(googledrive)
 
-## Make needed folder(s)
-dir.create(file.path("data"), showWarnings = F)
-dir.create(file.path("data", "raw"), showWarnings = F)
-
 # Identify wanted files
-files_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Sw-CdVIsCNvnS3laPn1a90WHoZsEoMif")) %>% 
+files_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/1/folders/1zI1KYBlROyBZSgjSEYmVjsIfCmRPpUPq")) %>% 
   dplyr::filter(stringr::str_detect(string = .$name, pattern = "\\.csv"))
 
 # Did that work?
-print(files_drive, n=30)
+print(files_drive, n=40)
 
-##ABS_RCREC
+# Identify local files
+files_local <- dir(path = file.path("data", "raw"))
+files_local
+
+# Overwrite local data files?
+update <- FALSE
+
+# Identify desired files
+if(update == T) {
+  files_wanted <- files_drive 
+} else {
+  files_wanted <- files_drive %>%
+    dplyr::filter(!name %in% files_local)
+}
+
+# Download them!
+purrr::walk2(.x = files_wanted$id, .y = files_wanted$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", "raw", .y)))
+
 # ABS_UF
+# Add site and network name
+abs <- read.csv("./data/raw/ABS_UF_BIR_NIFA_ANPP_Betsey.csv")
+abs$site <- "abs_ltar"
+
+write.csv(abs, "")
+
+rm(list = ls()); gc()
+
 # CAF
+# Add site and network name
+caf <- read.csv("./data/raw/CAF_DET_20231218_MeasHarvestFraction.csv")
+caf$site <- "caf_ltar"
+
+write.csv(caf, "")
+
+rm(list = ls()); gc()
+
 # CPER
-# ECB_E1
-# ECB_D2
-# ECB_C1
-# GB_wbs
-# GB_los
-# GB_mbs
-# JER
+cper <- read.csv("./data/raw/CPER_LTNPP_MeasGrazingPlants.csv")
+cper$site <- "cper_ltar"
+
+write.csv(cper, "")
+
+rm(list = ls()); gc()
+
+# ECB
+
+
+# GB
 # KBS
+kbs <- read.csv("./data/raw/KBS_ANPP.csv")
+kbs$site <- "kbs_ltar"
+
+write.csv(kbs, "")
+
+rm(list = ls()); gc()
+
 # LCB
+lcb <- read.csv("./data/raw/CAF_DET_20231218_MeasHarvestFraction.csv")
+lcb$site <- "lcb_ltar"
+
+write.csv(lcb, "")
+
+rm(list = ls()); gc()
+
 # NH
+nh <- read.csv("./data/raw/UMRB_MeasHarvestFrac_v2.csv")
+nh$site <- "nh_ltar"
+
+write.csv(nh, "")
+
+rm(list = ls()); gc()
+
 # NP_C
+caf <- read.csv("./data/raw/CAF_DET_20231218_MeasHarvestFraction.csv")
+caf$site <- "caf_ltar"
+
+write.csv(abs, "")
+
+rm(list = ls()); gc()
+
 # NP_R
-# PRHPA
+caf <- read.csv("./data/raw/CAF_DET_20231218_MeasHarvestFraction.csv")
+caf$site <- "caf_ltar"
+
+write.csv(abs, "")
+
+rm(list = ls()); gc()
+
+# PRHPA NEMERREN
+# PRHPA NEMELTCRS
 # SP
 # TG
-# UCB
+# UCB - Not doing now
 # UMRB
