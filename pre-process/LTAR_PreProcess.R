ec90919631b66ffcb8e098af926ee493a45c9caf
oliviahaj <olhajek@gmail.com>
2025-02-26 10:07:54 -0800

    Updating site name for LTAR

--- pre-process/LTAR_PreProcess.R
+++ pre-process/LTAR_PreProcess.R
@@ -40,48 +40,48 @@ purrr::walk2(.x = files_wanted$id, .y = files_wanted$name,
 # ABS_UF
 # Add site and network name
 abs <- read.csv("./data/raw/ABS_UF_BIR_NIFA_ANPP_Betsey.csv")
-abs$site <- "abs_ltar"
+abs$site <- "ABS_UF_LTAR"
 
-write.csv(abs, "./data/pre_processed_data/ABS_UF_BIR_NIFA_ANPP_Betsey.csv")
+write.csv(abs, "./data/pre_processed_data/ABS_UF_BIR_NIFA_ANPP_Betsey.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # CAF
 # Add site and network name
 caf <- read.csv("./data/raw/CAF_DET_20231218_MeasHarvestFraction.csv")
-caf$site <- "caf_ltar"
+caf$site <- "CAF_LTAR"
 
 ##Select only AGB
 # caf2 <- caf %>%
 #   filter(Plant.Fraction == "Aboveground biomass")
 
-write.csv(caf, "./data/pre_processed_data/CAF_DET_20231218_MeasHarvestFraction.csv")
+write.csv(caf, "./data/pre_processed_data/CAF_DET_20231218_MeasHarvestFraction.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # CPER
 # Add site and network name
 cper <- read.csv("./data/raw/CPER_LTNPP_MeasGrazingPlants.csv")
-cper$site <- "cper_ltar"
+cper$site <- "CPER_LTAR"
 
 # Filter out shrubs (SHRB) and sub-shrubs (SS)
 cper2 <- cper %>%
   filter(!Functional.Groups %in% c("SS", "SHRB"))
 
-write.csv(cper2, "./data/pre_processed_data/CPER_LTNPP_MeasGrazingPlants.csv")
+write.csv(cper2, "./data/pre_processed_data/CPER_LTNPP_MeasGrazingPlants.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # ECB
 # Add site and network name
 ecb <- read.csv("./data/raw/ECB_MeasHarvestFractionv2.csv")
-ecb$site <- "ecb_ltar"
 
+ecb$site <- paste(ecb$Unit.ID, "LTAR", sep="_")
 ##Fix treatment names (ECB_B1bau - ECB_E1BAU)
 ##Although, we won't use this treatment because it only has grain yield
 ecb$Treatment.ID <- ifelse(ecb$Treatment.ID =="ECB_B1BAU", "ECB_E1BAU", ecb$Treatment.ID)
 
-write.csv(ecb, "./data/pre_processed_data/ECB_MeasHarvestFractionv2.csv")
+write.csv(ecb, "./data/pre_processed_data/ECB_MeasHarvestFractionv2.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
@@ -100,20 +100,20 @@ gb$site <- gb$Treatment.ID
 gb2 <- gb %>%
   filter(Functional.Groups!="litter")
 
-write.csv(gb2, "./data/pre_processed_data/GB_MeasGrazingPlants_04242024.csv")
+write.csv(gb2, "./data/pre_processed_data/GB_MeasGrazingPlants_04242024.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # KBS
 # Add site and network name
 kbs <- read.csv("./data/raw/KBS_ANPP.csv")
-kbs$site <- "kbs_ltar"
+kbs$site <- "KBS_LTAR"
 
 # BIOMASS IS IN G/M2
 # Update error in treatment name (KBS_T21 should be T2)
 kbs$Treatment.ID <- ifelse(kbs$Treatment.ID =="KBS_T21", "KBS_T2", kbs$Treatment.ID)
 
-write.csv(kbs, "./data/pre_processed_data/KBS_ANPP.csv")
+write.csv(kbs, "./data/pre_processed_data/KBS_ANPP.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
@@ -122,58 +122,58 @@ rm(list = ls()); gc()
 # NH
 # Add site and network name
 nh <- read.csv("./data/raw/UMRB_MeasHarvestFrac_v2.csv")
-nh$site <- "nh_ltar"
+nh$site <- "NH_LTAR"
 
-write.csv(nh, "./data/pre_processed_data/UMRB_MeasHarvestFrac_v2.csv")
+write.csv(nh, "./data/pre_processed_data/UMRB_MeasHarvestFrac_v2.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # NP_C
 # Add site and network name
 np_c <- read.csv("./data/raw/NPMA_MeasHarvestFraction.csv")
-np_c$site <- "np_c_ltar"
+np_c$site <- "NP_C_LTAR"
 
-write.csv(np_c, "./data/pre_processed_data/NPMA_MeasHarvestFraction.csv")
+write.csv(np_c, "./data/pre_processed_data/NPMA_MeasHarvestFraction.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # NP_R
 np_r <- read.csv("./data/raw/NP_InOut_MeasGrazingPlants.csv")
-np_r$site <- "np_r_ltar"
+np_r$site <- "NP_R_LTAR"
 
-write.csv(np_r, "./data/pre_processed_data/NP_InOut_MeasGrazingPlants.csv")
+write.csv(np_r, "./data/pre_processed_data/NP_InOut_MeasGrazingPlants.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # PRHPA NEMERREN
 prhpa.nem <- read.csv("./data/raw/PRHPA_NEMERREM_MeasHarvestFraction.csv")
-prhpa.nem$site <- "prhpa_ltar"
+prhpa.nem$site <- "PRHPA_LTAR"
 
-write.csv(prhpa.nem, "./data/pre_processed_data/PRHPA_NEMERREM_MeasHarvestFraction.csv")
+write.csv(prhpa.nem, "./data/pre_processed_data/PRHPA_NEMERREM_MeasHarvestFraction.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # PRHPA NEMELTCRS
 prhpa.nemel <- read.csv("./data/raw/PRHPA_NEMELTCRS_MeasResidueMgnt.csv")
-prhpa.nemel$site <- "prhpa_ltar"
+prhpa.nemel$site <- "PRHPA_LTAR"
 
-write.csv(prhpa.nemel, "./data/pre_processed_data/PRHPA_NEMELTCRS_MeasResidueMgnt.csv")
+write.csv(prhpa.nemel, "./data/pre_processed_data/PRHPA_NEMELTCRS_MeasResidueMgnt.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # SP
 sp <- read.csv("./data/raw/SP_RotGraz_MeasGrazingPlants_OLH.csv")
-sp$site <- "sp_ltar"
+sp$site <- "SP_LTAR"
 
-write.csv(sp, "./data/pre_processed_data/SP_RotGraz_MeasGrazingPlants_OLH.csv")
+write.csv(sp, "./data/pre_processed_data/SP_RotGraz_MeasGrazingPlants_OLH.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # TG
 tg <- read.csv("./data/raw/TG_GSWRL_LTBE_MeasHarvestFractionv2.csv")
-tg$site <- "tg_ltar"
+tg$site <- "TG_LTAR"
 
-write.csv(tg, "./data/pre_processed_data/TG_GSWRL_LTBE_MeasHarvestFractionv2.csv")
+write.csv(tg, "./data/pre_processed_data/TG_GSWRL_LTBE_MeasHarvestFractionv2.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
@@ -181,13 +181,16 @@ rm(list = ls()); gc()
 
 # UMRB
 umrb <- read.csv("./data/raw/UMRB_AMES_IAKFT_MeasHarvestFraction.csv")
-umrb$site <- "umrb_ltar"
+umrb$site <- "UMRB_LTAR"
 
-write.csv(umrb, "./data/pre_processed_data/UMRB_AMES_IAKFT_MeasHarvestFraction.csv")
+write.csv(umrb, "./data/pre_processed_data/UMRB_AMES_IAKFT_MeasHarvestFraction.csv", row.names=FALSE)
 
 rm(list = ls()); gc()
 
 # Move the pre_processed_data folder up to the drive
+files_upload <- dir(path = file.path("data", "pre_processed_data"))
+files_upload <- data.frame(files_upload)
 
-
-
+purrr::walk2(.x = file.path("data", "pre_processed_data", files_upload$files_upload), .y = files_upload$files_upload,
+             .f = ~ googledrive::drive_upload(file = .x, overwrite = T,
+                                                path = file.path("data", "olh_test", .y)))
