d483b790d4af028b7c6057c9e35f6cea516c2d14
oliviahaj <olhajek@gmail.com>
2025-02-27 14:40:17 -0800

    Annual precip join and check. QA/QC

--- 02_quality-control.R
+++ 02_quality-control.R
@@ -51,6 +51,12 @@ tidy_v2 <- tidy_v1 %>%
 network_nas <- tidy_v2 %>%
   filter(is.na(network))
 
+# Fix site name for PRHPA
+tidy_v2 <- tidy_v2 %>%
+  dplyr::mutate(site = ifelse(site=="PRHPA_NEMELTCRS", "PRHPA", site))%>%
+  dplyr::mutate(site = ifelse(site=="PRHPA_NEMERREM", "PRHPA", site)) %>%
+  dplyr::mutate(site = ifelse(site=="jrn", "JRN", site)) %>%
+  dplyr::mutate(site = ifelse(site=="sev", "SEV", site))
 ## --------------------------------------- ##
 # Fix Dates and Year----
 ## --------------------------------------- ##
@@ -80,29 +86,26 @@ dplyr::glimpse(tidy_v4)
 # Calculate duration for each "site"
 #MEGHAN NOTES: year is not filled out for each dataset so this isn't working, i created a new year2 column above, it works now.
 
-tidy_v5 <- tidy_v4 %>% 
-  dplyr::group_by(source) %>% 
-  dplyr::mutate(duration_years = length(unique(year2)),
-                .after = source) %>% 
-  dplyr::ungroup() %>% 
-  filter(!is.na(year2)) 
+tidy_v4 <- tidy_v3 %>% 
+  dplyr::group_by(site, treatment) %>% 
+  dplyr::mutate(duration_years = length(unique(year))) %>% 
+  dplyr::ungroup()
 
 # Check structure
-dplyr::glimpse(tidy_v5)
+dplyr::glimpse(tidy_v4)
 
 ## ------------------------------------------- ##
 # ANPP Unit Conversions ----
 ## ------------------------------------------- ##
-
-tidy_v2 <- tidy_v1 %>%
+tidy_v5 <- tidy_v4 %>%
   # ANPP - convert KG/ha to g/m2
   dplyr::mutate(anpp_g_m2 = ifelse(is.na(anpp_g_m2), anpp_kg_ha/10, anpp_g_m2))
 
 # Check to make sure only unwanted columns are lost
-supportR::diff_check(old = names(tidy_v5), new = names(tidy_v6))
+supportR::diff_check(old = names(tidy_v4), new = names(tidy_v5))
 
 # Check structure
-dplyr::glimpse(tidy_v6)
+dplyr::glimpse(tidy_v5)
 
 ## ------------------------------------------- ##
 # Download Precip Data ----
@@ -134,44 +137,20 @@ precip_v1 <- read.csv(file = file.path("data", "environment", "precip_annual-sum
 # Check structure
 dplyr::glimpse(precip_v1)
 
-# Do needed wrangling (if any)
-precip_v2 <- precip_v1 %>%
-  dplyr::mutate(treatment = ifelse(network == "LTAR", yes = site, no = NA)) %>% 
-  dplyr::select(-network, -mean_daily_precip_mm) %>% 
-  dplyr::distinct()
-
-# Re-check structure
-dplyr::glimpse(precip_v2)
-
-# Split by network
-ltar_ppt <- precip_v2 %>% 
-  dplyr::filter(!is.na(treatment)) %>% 
-  dplyr::rename(total_annual_precip_mm.ltar = total_annual_precip_mm) %>% 
-  dplyr::select(-site) %>% 
-  dplyr::distinct()
-nutnet_ppt <- precip_v2 %>% 
-  dplyr::filter(is.na(treatment)) %>% 
-  dplyr::rename(total_annual_precip_mm.nutnet = total_annual_precip_mm) %>% 
-  dplyr::select(-treatment) %>% 
-  dplyr::distinct()
-
 ## ------------------------------------------- ##
 # Attach Precip Data ----
 ## ------------------------------------------- ##
+tidy_v6 <- left_join(tidy_v5, precip_v1, by=c("network", "site","year"))
 
-# Attach to 'actual' data
-tidy_v7 <- tidy_v5 %>% 
-  rename(year=year2) %>% 
-  mutate(year=as.integer(year)) %>% 
-  dplyr::left_join(x = ., y = ltar_ppt, by = c("treatment", "year")) %>% 
-  dplyr::left_join(x = ., y = nutnet_ppt, by = c("site", "year")) %>% 
-  # Coalesce values
-  dplyr::mutate(total_annual_precip_mm = dplyr::coalesce(total_annual_precip_mm.ltar, 
-                                                         total_annual_precip_mm.nutnet),
-                .after = date) %>% 
-  # Drop superseded columns
-  dplyr::select(-dplyr::starts_with("total_annual_precip_mm."))
-  
+# Check missing precip data
+ppt_check <- tidy_v6 %>%
+  filter(is.na(total_annual_precip_mm))%>%
+  group_by(site, treatment) %>%
+  count()
+
+# Fix network capitilz
+
+
 # Check structure
 dplyr::glimpse(tidy_v7)
 
