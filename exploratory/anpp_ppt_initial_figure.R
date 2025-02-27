b2f5093f4c42db09c86749d6491c6bb889ea50d5
oliviahaj <olhajek@gmail.com>
2025-02-27 14:54:15 -0800

    anpp_ppt figure with wrangled data from step 02

---
+++ exploratory/anpp_ppt_initial_figure.R
@@ -0,0 +1,30 @@
+###Playing around with roughly harmonized adn processed data
+
+# Identify nice name for exported object
+focal_output <- "02_resilience_wrangled.csv"
+
+# Export locally
+anpp <- read.csv(file = file.path("data", "tidy", focal_output))
+
+str(combo_ppt)
+
+ggplot(subset(anpp, anpp$network != "LTAR and LTER"), aes(total_annual_precip_mm, anpp_g_m2, color=network))+
+  geom_point()+
+  ylim(0, 3000)+
+  theme_bw()+
+  facet_wrap(~network)
+
+##summarize
+str(anpp)
+
+agg <- anpp %>%
+  group_by(site, location, treatment, year, total_annual_precip_mm, network) %>%
+  summarize(mean.anpp = mean(anpp_g_m2))
+
+ggplot(
+  subset(agg, agg$network != "LTAR and LTER"), 
+  aes(total_annual_precip_mm, mean.anpp, color=network))+
+  geom_point()+
+  ylim(0, 3000)+
+  theme_bw()+
+  facet_wrap(~network)
