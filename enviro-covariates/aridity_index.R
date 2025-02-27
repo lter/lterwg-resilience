6d98297c985e13cb2cac732ec3955f83f0a8a385
LinaAoyama <lina.peninsula@gmail.com>
2025-02-27 10:02:15 -0800

    trouble shooting SPEI package

--- enviro-covariates/aridity_index.R
+++ enviro-covariates/aridity_index.R
@@ -8,6 +8,11 @@ library(SPEI)
 
 #download climate data and site info (see weatherSiteInitialFigure.R)
 #merge climate and site tables
-climate_site <- left_join(site_info, maw) %>%
-  mutate(PET = SPEI::thornthwaite(climate_site$mean_tmean_degC, climate_site$latitude, na.rm = FALSE))#calculate PET
+climate_site <- left_join(site_info, maw) 
+climate_site_sort <- climate_site[order(climate_site$latitude, decreasing = TRUE), ]
+aridity_site <- climate_site_sort %>%
+  mutate(PET = SPEI::thornthwaite(mean_tmean_degC, latitude, na.rm = FALSE))#calculate PET
 
+SPEI::thornthwaite(Tave = 10.43997267, lat = 48.2065)
+
+climate_site$spei<-thornthwaite(climate_site$mean_tmean_degC, climate_site$latitude)
