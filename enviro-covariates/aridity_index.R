90da5f77e142c90b87fc514e92e6652d9423352f
LinaAoyama <lina.peninsula@gmail.com>
2025-02-27 10:11:16 -0800

    someone else help me calculate PET for me!

--- enviro-covariates/aridity_index.R
+++ enviro-covariates/aridity_index.R
@@ -8,11 +8,5 @@ library(SPEI)
 
 #download climate data and site info (see weatherSiteInitialFigure.R)
 #merge climate and site tables
-climate_site <- left_join(site_info, maw) 
-climate_site_sort <- climate_site[order(climate_site$latitude, decreasing = TRUE), ]
-aridity_site <- climate_site_sort %>%
-  mutate(PET = SPEI::thornthwaite(mean_tmean_degC, latitude, na.rm = FALSE))#calculate PET
-
-SPEI::thornthwaite(Tave = 10.43997267, lat = 48.2065)
-
-climate_site$spei<-thornthwaite(climate_site$mean_tmean_degC, climate_site$latitude)
+climate_site <- left_join(site_info, maw) %>%
+  mutate(thornthwaite(climate_site$mean_tmean_degC, climate_site$latitude, na.rm = FALSE))
