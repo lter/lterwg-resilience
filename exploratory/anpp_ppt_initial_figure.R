92d5319da2b30a9def05a093b42e5b23d90f21a2
oliviahaj <olhajek@gmail.com>
2025-02-27 15:59:43 -0800

    Update figure to save to google drive

--- exploratory/anpp_ppt_initial_figure.R
+++ exploratory/anpp_ppt_initial_figure.R
@@ -21,10 +21,21 @@ agg <- anpp %>%
   group_by(site, location, treatment, year, total_annual_precip_mm, network) %>%
   summarize(mean.anpp = mean(anpp_g_m2))
 
-ggplot(
+g1 <- ggplot(
   subset(agg, agg$network != "LTAR and LTER"), 
   aes(total_annual_precip_mm, mean.anpp, color=network))+
   geom_point()+
   ylim(0, 3000)+
   theme_bw()+
   facet_wrap(~network)
+
+#create necessary folder:
+dir.create(file.path("data", "exploratory_graphs"), showWarnings = F)
+#save figure locally
+ggsave(filename = file.path("data", "exploratory_graphs", "annual_ppt_anpp_graph_OLH.png"),
+       plot = g1, width = 12, height = 6, units = "in", dpi = 300)
+
+#upload figure to google drive
+googledrive::drive_upload(media = file.path("data", "exploratory_graphs", "annual_ppt_anpp_graph_OLH.png"), overwrite = T,
+                          path = googledrive::as_id(dir.exploratory_graphs))
+
