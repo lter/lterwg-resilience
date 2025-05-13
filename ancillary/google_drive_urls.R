d9959bd1290164cc5c3a579584a57288a272d585
kemullerDRIVES <=>
2025-05-13 16:20:54 -0400

    updated directory names

--- ancillary/google_drive_urls.R
+++ ancillary/google_drive_urls.R
@@ -1,8 +1,8 @@
 ## Google drive URLS for data folders.
 # URL for the main data folder
 dir.data = "https://drive.google.com/drive/folders/1Ty7QX7vyvD797eKJzMWbr8AwIo-GyBFO"
-## data/tidy_data
-dir.tidy_data = "https://drive.google.com/drive/folders/13Ymkrr-kRLDmpaj1jwwVOnOSmEYnF-dJ"
+## data/harmonized_data
+dir.harmonized_data = "https://drive.google.com/drive/folders/13Ymkrr-kRLDmpaj1jwwVOnOSmEYnF-dJ"
 
 ## data/raw_data
 dir.raw_data = "https://drive.google.com/drive/folders/1zI1KYBlROyBZSgjSEYmVjsIfCmRPpUPq"
