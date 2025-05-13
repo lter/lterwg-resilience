ad7db6b6350f57a640bc5c812cf972e1fb98b352
kemullerDRIVES <=>
2025-05-13 13:21:34 -0400

    updated create folder script

--- 00_create_data_folder.R
+++ 00_create_data_folder.R
@@ -1,6 +1,6 @@
 # RUN THIS SCRIPT BEFORE RUNNING ANY ANALYSIS
 # Make needed folder(s)
 dir.create(file.path("data"), showWarnings = F)
-dir.create(file.path("data", "raw"), showWarnings = F)
+dir.create(file.path("data", "raw_data"), showWarnings = F)
 dir.create(file.path("data", "pre_processed_data"), showWarnings = F)
-dir.create(file.path("data", "tidy_data"), showWarnings = F)
+dir.create(file.path("data", "harmonized_data"), showWarnings = F)
