eadfc759bb2cc6d863fea84daf6737e6e7a35964
njlyon0 <nicholasjlyon@gmail.com>
2025-07-01 12:19:14 -0400

    feat (whiplash): adding ANPP coverage to diagnostic graphs

--- enviro-covariates/whiplash-windows_spei.R
+++ enviro-covariates/whiplash-windows_spei.R
@@ -17,6 +17,7 @@ librarian::shelf(tidyverse, googledrive, supportR)
 
 # Make needed folder(s)
 dir.create(file.path("data", "environment"), showWarnings = F, recursive = T)
+dir.create(file.path("data", "harmonized_data"), showWarnings = F, recursive = T)
 dir.create(file.path("data", "diagnostic", "whiplash"), showWarnings = F, recursive = T)
 dir.create(file.path("graphs", "diagnostic", "whiplash"), showWarnings = F, recursive = T)
 
@@ -28,7 +29,7 @@ purrr::walk(.x = dir(path = file.path("tools"), pattern = "fxn_"),
             .f = ~ source(file.path("tools", .x)))
 
 ## ------------------------------------- ##
-# Download SPEI CSVs ----
+# Download SPEI Data ----
 ## ------------------------------------- ##
 # Created by "exploratory/download_SPEI-site.R"
 
@@ -46,7 +47,7 @@ drive_spei
 #                                                 path = file.path("data", "environment", .y)))
 
 ## ------------------------------------- ##
-# Preparatory Wrangling ----
+# Preparatory SPEI Wrangling ----
 ## ------------------------------------- ##
 
 # Identify desired SPEI file
@@ -71,6 +72,42 @@ spei_v2 <- spei_v1 %>%
 
 # Re-check structure
 dplyr::glimpse(spei_v2)
+
+## ------------------------------------- ##
+# Download ANPP Data ----
+## ------------------------------------- ##
+
+# This is non-critical to actual whiplash identification
+## But will be nice context for the diagnostic plots
+
+drive_anpp <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/13Ymkrr-kRLDmpaj1jwwVOnOSmEYnF-dJ")) %>% 
+  dplyr::filter(stringr::str_detect(string = name, pattern = ".csv")) %>% 
+  dplyr::filter(name == "02_anpp_filter1.csv")
+
+# Check that worked
+drive_anpp
+
+# Download just the relevant file
+## Uncomment to re-download
+# googledrive::drive_download(file = drive_anpp$id, overwrite = T,
+#                             path = file.path("data", "harmonized_data", drive_anpp$name))
+
+## ------------------------------------- ##
+# Preparatory ANPP Wrangling ----
+## ------------------------------------- ##
+
+# Read that in
+anpp_v1 <- read.csv(file.path("data", "harmonized_data", drive_anpp$name))
+
+# Check structure
+dplyr::glimpse(anpp_v1)
+
+# For this script, we only want the max/min years
+anpp_extent <- c(min(anpp_v1$year, na.rm = T),
+                 max(anpp_v1$year, na.rm = T))
+
+# Check that out
+anpp_extent
 
 ## ------------------------------------- ##
 # Identify Whiplash Events ----
@@ -131,6 +168,14 @@ for(focal_site in sort(unique(spei_v2$site))){
                color = "blue", linewidth = 0.5) +
     geom_hline(yintercept = unique(whiplash_df$whiplash_thresh_lower), linetype = 2, 
                color = "blue", linewidth = 0.5) +
+    # Annotate ANPP coverage
+    annotate("rect", ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "gray45",
+             xmin = as.Date(x = paste0(min(anpp_extent), "-01-01")),
+             xmax = as.Date(x = paste0(max(anpp_extent), "-12-31"))) +
+    geom_text(label = "ANPP Coverage", size = 4, 
+              y = max(whiplash_df$SPEI_diff) - (max(whiplash_df$SPEI_diff) * 0.025),
+              x = as.Date(paste0(ceiling((max(anpp_extent) - min(anpp_extent)) / 2) + min(anpp_extent), "-01-01"))) +
+    # Final cosmetic stuff
     labs(x = "Date", y = "SPEI Differences") +
     supportR::theme_lyon()
   
@@ -145,8 +190,14 @@ for(focal_site in sort(unique(spei_v2$site))){
     geom_point(size = 0.5, color = "black") + 
     geom_point(data = whiplash_only, aes(x = date, y = SPEI),
                color = "red", size = 2) +
-    geom_point(data = whiplash_only, aes(x = prior_date, y = prior_SPEI),
-               color = "blue", size = 2) +
+    # geom_point(data = whiplash_only, aes(x = prior_date, y = prior_SPEI),
+    #            color = "blue", size = 2) +
+    annotate("rect", ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "gray45",
+             xmin = as.Date(x = paste0(min(anpp_extent), "-01-01")),
+             xmax = as.Date(x = paste0(max(anpp_extent), "-12-31"))) +
+    geom_text(label = "ANPP Coverage", size = 4, 
+              y = max(whiplash_df$SPEI) - (max(whiplash_df$SPEI) * 0.005),
+              x = as.Date(paste0(ceiling((max(anpp_extent) - min(anpp_extent)) / 2) + min(anpp_extent), "-01-01"))) +
     labs(x = "Date", y = "SPEI") +
     supportR::theme_lyon() +
     theme(legend.position = "none")
