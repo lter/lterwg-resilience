221e3fffbd143b0522a45267a1bf0ffafb7da806
oliviahaj <olhajek@gmail.com>
2025-08-24 10:25:26 -0600

    Make some summary boxplots of the ANPP and precip data

--- exploratory/anpp_summary_visualizations.R
+++ exploratory/anpp_summary_visualizations.R
@@ -11,7 +11,7 @@
 ## --------------------------------------- ##
 
 # Load libraries
-librarian::shelf(tidyverse, googledrive, supportR, cowplot, plyr)
+librarian::shelf(plyr, tidyverse, googledrive, supportR, cowplot)
 
 # Make needed folder(s)
 dir.create(file.path("data"), showWarnings = F)
@@ -47,16 +47,16 @@ unique(anpp_v1$crop)
 # Summarize data by mean and variability
 str(anpp_v1)
 anpp_sum <- anpp_v1 %>%
-  group_by(site, network, treatment, crop) %>%
-  summarize(mean = mean(anpp_g_m2, na.rm=TRUE),
+  dplyr::group_by(site, network, treatment, crop) %>%
+  dplyr::summarize(mean = mean(anpp_g_m2, na.rm=TRUE),
             sd = sd(anpp_g_m2, na.rm=TRUE),
             cv = (sd/mean) * 100,
             n = length(anpp_g_m2)) %>%
   # Filter out sites with NA means (grain yield only)
-  filter(!is.na(mean)) %>%
+  dplyr::filter(!is.na(mean)) %>%
   # Filter out observations with fewer than 5 years of data?
-  filter(n > 4) %>%
-  mutate(network =ifelse(network=="LTAR and LTER", "LTER", network))
+  dplyr::filter(n > 4) %>%
+  dplyr::mutate(network =ifelse(network=="LTAR and LTER", "LTER", network))
 
 # Make some visualizations
 hists.mean <- ggplot(anpp_sum, aes(mean, group=network))+
@@ -80,15 +80,16 @@ anpp_sum2 <- anpp_v1 %>%
             sd = sd(anpp_g_m2, na.rm=TRUE),
             cv = (sd/mean) * 100,
             n = length(anpp_g_m2), 
-            MAP = mean(wyr_ppt), 
-            cv_ppt = (sd(wyr_ppt)/MAP) * 100) %>%
+            map = mean(wyr_ppt), 
+            cv_ppt = (sd(wyr_ppt)/map) * 100) %>%
   # Filter out sites with NA means (grain yield only)
   dplyr::filter(!is.na(mean)) %>%
   # Filter out observations with fewer than 5 years of data?
   dplyr::filter(n > 2) %>%
   dplyr::mutate(network =ifelse(network=="LTAR and LTER", "LTER", network))%>%
   # Round precipitation to the nearest 100
-  dplyr::mutate(MAP_hund = round_any(MAP, 100, f= ceiling))
+  dplyr::mutate(MAP_hund = round_any(map, 100, f= ceiling)) %>%
+  dplyr::mutate(CV_ten = round_any(cv_ppt, 10, f= ceiling))
 
 boxes <- ggplot(anpp_sum2, aes(MAP_hund, mean, group=MAP_hund))+
   geom_boxplot()+
@@ -98,4 +99,96 @@ boxes <- ggplot(anpp_sum2, aes(MAP_hund, mean, group=MAP_hund))+
   ylab("Mean ANPP")+
   ggtitle("Mean ANPP (by site, treatment, network) vs Mean PPT Rounded to Nearest 100mm")
 boxes
+
+boxes.2 <- ggplot(anpp_sum2, aes(CV_ten, cv, group=CV_ten))+
+  geom_boxplot()+
+  geom_jitter(aes(color=network), alpha=0.5)+
+  theme_bw()+
+  xlab("CV (nearest 10th%)")+
+  ylab("CV of ANPP")+
+  ggtitle("ANPP CV (by site, treatment, network) vs Mean CV Rounded to Nearest 100mm")
+boxes.2
+
+##Look at which sites have the most variable precipitation in this window
+var <- anpp_sum2 %>%
+  filter(cv_ppt>40)
+
+
+### LOOK AT PRECIP Variation for the ANPP record length versus for 30 years
+##ANPP record length
+
+anpp_sum3 <- anpp_v1 %>%
+  dplyr::group_by(site, network) %>%
+  dplyr::summarize(
+                   n = length(anpp_g_m2), 
+                   map = mean(wyr_ppt), 
+                   cv_ppt = (sd(wyr_ppt)/map) * 100) %>%
+  # Filter out observations with fewer than 5 years of data?
+  dplyr::filter(n > 2) %>%
+  dplyr::mutate(network =ifelse(network=="LTAR and LTER", "LTER", network))
+
+anpp.rec.length <- ggplot(anpp_sum3, aes(map, cv_ppt, color=network))+
+  geom_point()+
+  theme_bw()+
+  xlim(200, 2000)+
+  ylim(5, 50)+
+  ylab("PPT CV (%) - ANPP Record Length")+
+  xlab("MAP (mm) - ANPP Record Length")
+anpp.rec.length
+
+#######################
+#Read in CLIMATE DATA 
+#######################
+
+# Identify desired file
+climate_data <- "site_climate_mswep.csv"
+
+# Download harmonized data file
+googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/13Ymkrr-kRLDmpaj1jwwVOnOSmEYnF-dJ")) %>% 
+  dplyr::filter(name == climate_data) %>% 
+  googledrive::drive_download(file = .$id, overwrite = T,
+                              path = file.path("data", "harmonized_data", .$name))
+
+# Read in harmonized data
+climate_file <- read.csv(file = file.path("data", "harmonized_data", climate_data)) %>%
+  dplyr::rename(site = site_id)
+
+# Check structure
+dplyr::glimpse(climate_file)
+
+###################################
+# Join Mean ANPP AND CLIMATE DATA #
+###################################
+merge_ANPP_climate <- left_join(anpp_sum3, climate_file, by = c("site"))
+rec.length.30 <- ggplot(merge_ANPP_climate, aes(MAP, cv_ppt_inter, color=network))+
+  geom_point()+
+  theme_bw()+
+  xlim(200, 2000)+
+  ylim(5, 50)+
+  ylab("PPT CV (%) - 30 year record")+
+  xlab("MAP (mm) - 30 year record")
+rec.length.30
+
+plot_grid(anpp.rec.length, rec.length.30)
+
+##Look at teh one to one lines
+map_graph <- ggplot(merge_ANPP_climate, aes(MAP, map, color=network))+
+  geom_point()+
+  theme_bw()+
+  geom_abline(slope=1)+
+  ylab("MAP (mm) - ANPP record")+
+  xlab("MAP (mm) - 30 year record")+
+  ggtitle("MAP")
+map_graph
+
+cv_graph <- ggplot(merge_ANPP_climate, aes(cv_ppt_inter, cv_ppt, color=network))+
+  geom_point()+
+  theme_bw()+
+  geom_abline(slope=1)+
+  ylab("CV - ANPP record")+
+  xlab("CV - 30 year record")+
+  ggtitle("CV")
+cv_graph
+
+plot_grid(map_graph, cv_graph)
 
