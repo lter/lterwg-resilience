555dba14159b798f64e51a3dfc2b90490b224c54
oliviahaj <olhajek@gmail.com>
2025-08-27 14:50:40 -0700

    Make sure ABS_UF is preserved in this processing step

--- anpp02_initial_filter.R
+++ anpp02_initial_filter.R
@@ -106,7 +106,7 @@ ltar <- harm_anpp.3 %>%
 
 # Create dataset without LTAR 
 nutnetlter <- harm_anpp.3 %>%
-  dplyr::filter(network %in% c("LTER", "lter","NutNet", "LTAR and LTER") | site == "CPER")
+  dplyr::filter(network %in% c("LTER", "lter","NutNet", "LTAR and LTER") | site == "CPER" | site=="ABS_UF")
 
 harm_anpp.4 <- rbind(ltar, nutnetlter)
 
