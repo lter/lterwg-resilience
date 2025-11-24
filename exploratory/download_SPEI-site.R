e30d8cbe2734aafacec218efe515353032bd787d
mkhorchani <makki.khorchani@gmail.com>
2025-11-24 15:53:35 -0600

    Updated spei downloader to use the new site_info (75 sites)

--- exploratory/download_SPEI-site.R
+++ exploratory/download_SPEI-site.R
@@ -4,7 +4,7 @@
 # Authors: Makki Khorchani
 
 # Purpose
-## Download SPEI 6 months data for all sites and save a data frame on the drive.
+## Download SPEI data for all sites and save a data frame on the drive.
 ## The SPEI NC files are downloaded from this website https://spei.csic.es/spei_database/#map_name=spei01#map_position=1475
 
 # Clear environment + collect garbage
@@ -17,43 +17,9 @@ library(ncdf4)
 library(readxl)
 library(purrr)
 
-
-
-# NOTE
-## This script assumes (1) access to the "LTER-WG_Resilience-Management" Shared Drive (2) authentication with R
-## For more information on authentication, see the following tutorial:
-### https://lter.github.io/scicomp/tutorial_googledrive-pkg.html
-
-# Identify wanted files
-files_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Sw-CdVIsCNvnS3laPn1a90WHoZsEoMif")) %>% 
-  dplyr::filter(stringr::str_detect(string = .$name, pattern = "\\.csv"))
-
-# Did that work?
-files_drive
-
-# Identify local files
-files_local <- dir(path = file.path("data", "pre_processed_data"))
-files_local
-
-# Overwrite local data files?
-update <- FALSE
-
-# Identify desired files
-if(update == T) {
-  files_wanted <- files_drive 
-} else {
-  files_wanted <- files_drive %>%
-    dplyr::filter(!name %in% files_local)
-}
-
-# Download them!
-purrr::walk2(.x = files_wanted$id, .y = files_wanted$name,
-             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
-                                                path = file.path("data", "pre_processed_data", .y)))
-
 # Grab the data key
 site_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Ty7QX7vyvD797eKJzMWbr8AwIo-GyBFO")) %>% 
-  dplyr::filter(name == "site_summary_info")
+  dplyr::filter(name == "site_summary_info.csv")
 
 # Did that work?
 site_drive
@@ -65,34 +31,34 @@ googledrive::drive_download(file = site_drive$id, overwrite = T, type = "csv",
 
 
 # Choose an output name for the final csv file  and the SPEI dataset you want to work with (the outputname and nc_filename should include the path to the right folder)
-output_name<-"SPEI12.csv"
-nc_filename<-"spei12.nc"
+output_name<-file.path("data", "spei_data","spei06.csv")
+nc_filename<-file.path("data", "spei_data","spei06.nc")
 
 
-# importing site data (coordinates and siteID)
+# importing site data (coordinates and siteID) #make sure you have the last updates site_summary_info file downloaded
 sites <- read.csv(file = file.path("data", "site_summary_info.csv"), header = TRUE)
 
 # Creating a leaflet map of the site locations
-# leaflet(data = sites) %>%
-#   addTiles() %>%  # Add default OpenStreetMap layer
-#   addCircleMarkers(
-#     lng = ~longitude,      # use the longitude column
-#     lat = ~latitude,       # use the latitude column
-#     radius = 5,            # point size
-#     color = "red",         # outline color
-#     fillColor = "red",     # fill color
-#     fillOpacity = 0.8,
-#     
-#     # Option 1: Show site ID when hovering (label)
-#     label = ~site_id,
-#     labelOptions = labelOptions(noHide = FALSE, textsize = "12px"),
-#     
-#     # Option 2: Show site ID when clicked (popup)
-#     popup = ~paste("Site ID:", site_id)
-#   )
+leaflet(data = sites) %>%
+  addTiles() %>%  # Add default OpenStreetMap layer
+  addCircleMarkers(
+    lng = ~longitude,      # use the longitude column
+    lat = ~latitude,       # use the latitude column
+    radius = 5,            # point size
+    color = "red",         # outline color
+    fillColor = "red",     # fill color
+    fillOpacity = 0.8,
+
+    # Option 1: Show site ID when hovering (label)
+    label = ~site_id,
+    labelOptions = labelOptions(noHide = FALSE, textsize = "12px"),
+
+    # Option 2: Show site ID when clicked (popup)
+    popup = ~paste("Site ID:", site_id)
+  )
 
 # Open the ncdf file containing the global SPEI data
-ncfile <- nc_open(file.path("data","spei_data", nc_filename))
+ncfile <- nc_open(nc_filename)
 
 # Extract the main variables
 spei_var <- ncvar_get(ncfile, "spei")  # shape typically [lon, lat, time]
@@ -150,11 +116,11 @@ for(i in seq_len(n_sites)) {
 
 
 # Export final dataset
-write.csv(final_df,file=file.path("data","spei_data", output_name),row.names = F)
+write.csv(final_df,file=output_name,row.names = F)
 
 
-# Upload them to the drive
+# Upload output file to the drive
 purrr::walk(.x = output_name,
-            .f = ~ googledrive::drive_upload(media = file.path("data", "spei_data", .x), overwrite = T, path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Sw-CdVIsCNvnS3laPn1a90WHoZsEoMif")))
+            .f = ~ googledrive::drive_upload(media = .x, overwrite = T, path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1JtFMD4IAizjNGd0wbLLZIBdgqnk4YR97")))
 
 
