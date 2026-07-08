091d0af893509aac4c9dc73d066ae8af7dbbc607
ingridslette <ingrid.slette@gmail.com>
2026-07-08 10:41:20 -0500

    commit

--- retrieve-mswep-ppt-data.R
+++ retrieve-mswep-ppt-data.R
@@ -23,14 +23,14 @@ gs4_auth()
 ## I'm adding statements here as I retrieve data for more and more sets of site
 
 # if reading in a csv from the Shared drive using pathname:
-sites <- read.csv("/Users/ingridslette/Library/CloudStorage/GoogleDrive-slett152@umn.edu/Shared drives/LTER-WG_Resilience-Management/data/raw_data/ltar_lter_site_coordinates.csv")
+sites <- read.csv('/Users/ingridslette/Library/CloudStorage/GoogleDrive-slett152@umn.edu/Shared drives/LTER-WG_Resilience-Management/data/raw_data/nutnet-site-coordinates.csv')
 
 # if reading in a gsheet from the Shared drive using pathname:
 sheet_url <- 'https://docs.google.com/spreadsheets/d/1XPiisvk_ftkZRkWWDXK9dTGyIKWZ6Q87cSE_UNog3pA/edit?gid=338942080#gid=338942080'
 sites <- read_sheet(sheet_url)
 
 View(sites)
-unique(sites$site_id)
+unique(sites$site_code)
 
 # make that file a SpatVector
 site <- sites %>% vect(geom = c("longitude", "latitude"), crs = "EPSG:4326")
@@ -49,7 +49,7 @@ ppt_daily <- terra::extract(r, site, bind = TRUE)
 
 df <- as.data.frame(ppt_daily)
 
-names(df) <- c("site_id", "project_id", paste0("precip_", time(r)))
+names(df) <- c("site_code", "project_id", paste0("precip_", time(r)))
 
 out <- pivot_longer(df, -c("site_id", "project_id"), names_to = "date",
                     values_to = "precip") %>% 
@@ -156,6 +156,53 @@ out$day <- substr(out$date, 9, 10)
 View(out)
 
 write.csv(out, file = "/Users/ingridslette/Library/CloudStorage/GoogleDrive-slett152@umn.edu/Shared drives/LTER-WG_Resilience-Management/data/raw_data/mswep-daily-ppt-isu-drainage-sites.csv")
+
+
+
+
+
+sites <- read.csv('/Users/ingridslette/Library/CloudStorage/GoogleDrive-slett152@umn.edu/Shared drives/LTER-WG_Resilience-Management/data/raw_data/nutnet-site-coordinates.csv')
+
+View(sites)
+unique(sites$site_code)
+
+# make that file a SpatVector
+site <- sites %>% vect(geom = c("longitude", "latitude"), crs = "EPSG:4326")
+
+# list all of the monthly mswep precip data files
+# change this to location to which you downloaded these files
+r_paths <- list.files("/Users/ingridslette/Desktop/mswep_daily",
+                      full.names = TRUE) %>% 
+  sort()
+
+# make that a SpatRaster
+r <- rast(r_paths)
+
+# extract daily precip data for each site
+ppt_daily <- terra::extract(r, site, bind = TRUE)
+
+df <- as.data.frame(ppt_daily)
+
+names(df) <- c("site_code", paste0("precip_", time(r)))
+
+out <- pivot_longer(df, -c("site_code"), names_to = "date",
+                    values_to = "precip") %>% 
+  mutate(date = str_replace(date, "^precip_", ""))
+
+# create a new column for the year
+out$year <- substr(out$date, 1, 4)
+
+# create a new column for the month
+out$month <- substr(out$date, 6, 7)
+
+# create a new column for the day
+out$day <- substr(out$date, 9, 10)
+
+View(out)
+
+unique(out$site_code)
+
+write.csv(out, file = "/Users/ingridslette/Library/CloudStorage/GoogleDrive-slett152@umn.edu/Shared drives/LTER-WG_Resilience-Management/data/raw_data/mswep-daily-ppt-nutnet-sites.csv")
 
 
 
